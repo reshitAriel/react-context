ab02564e3eecf05c04942f1ba6676b1578c3c801
Xneti <nati34g@gmail.com>
2022-10-29 16:02:37 +0300

    update react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import UsernameChanger from "./components/UsernameChanger";
 import UsernameDisplay from "./components/UsernameDisplay";
 import Buttons from "./components/Buttons";
+// import React from 'reasct'
 
 import './App.css';
+import React from 'react';
 
 const App = () => {
   return (
